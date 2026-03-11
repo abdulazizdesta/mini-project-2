eeb9e6a87c36474a0c2708f46e5525bacff6c94d
Desta Aziz <abdulazizdesta@gmail.com>
2026-03-11 09:43:16 +0700

    mengubah arrow function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const hamburger = document.getElementById('hamburger');
 const listNavbar = document.getElementById('list-navbar-mobile');
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', function() {
   listNavbar.classList.toggle('active');
 });
